9f0132529d10b165915462f0019c464639f5bcad
hashmi mohammed <hashmi.smf@gmail.com>
2024-05-06 19:43:35 +0530

    removing modal properly via stimulus and turbo form submission actions

--- app/javascript/controllers/turbomodal_controller.js
+++ app/javascript/controllers/turbomodal_controller.js
@@ -5,4 +5,15 @@ export default class extends Controller {
   connect() {
     console.log("turbo modal connected");
   }
+
+  submitEnd(e) {
+    console.log("e", e);
+    if (e.detail.success) {
+      this.hideModal();
+    }
+  }
+
+  hideModal() {
+    this.element.remove();
+  }
 }
